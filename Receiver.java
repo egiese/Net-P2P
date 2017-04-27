c74e4038950d5c1af2e129bef2946d5b3c3b7f24
Simon Yawin <simyawin@gmail.com>
2017-04-27 00:57:15 -0400

    Added createACK method

--- Receiver.java
+++ Receiver.java
@@ -3,5 +3,19 @@
  */
 interface Receiver
 {
-    
+//    SEQ Number Info
+    int INIT_SEQ_NUM = 0;
+    int SEQ_NUM_WIN = 2;
+
+    /*
+	 * ------------------------------------------------------------------------------------
+	 * Method to create an ACK for a received packed based on its sequence number
+	 * Takes sequence number as parameter, and returns a String with the ACK flag raised,
+	 * the sequence number it's ACKing, and the message-terminating CRLFCRLF
+	 * ------------------------------------------------------------------------------------
+	 */
+    static String createACK(int sequenceNumber)
+    {
+        return " 1 " + sequenceNumber + "\r\n\r\n";
+    }
 }
