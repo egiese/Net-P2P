0189a3061c13c7856ab515800d03e07f98ff820f
Simon Yawin <simyawin@gmail.com>
2017-04-26 20:21:48 -0400

    Modify launcher skeleton

--- Launcher.java
+++ Launcher.java
@@ -5,23 +5,16 @@ import java.util.Scanner;
  */
 public class Launcher
 {
-    private Scanner scanner;
-    private String instanceType,hostName;
-    private int port;
 
     public static void main(String[] args)
     {
-        Launcher launcher = new Launcher();
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Welcome to the P2P application.\nIs this machine a client [c] or a server [s]?");
+        String instanceType = scanner.next();
+        System.out.println("What port number will be used for communication?");
+        int port = scanner.nextInt();
+        System.out.println("What is this hostname of this machine?");
+        String hostName = scanner.next();
     }
 
-    public Launcher() {
-        scanner = new Scanner(System.in);
-        System.out.println("Welcome to the P2P application.\nIs this machine a client [c] or a server [s]?");
-        instanceType = scanner.next();
-        System.out.println("What port number will be used for communication?");
-        port = scanner.nextInt();
-        System.out.println("What is this hostname of this machine?");
-        hostName = scanner.next();
-        System.out.println(instanceType + hostName + port);
-    }
 }
