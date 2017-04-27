7e4e5605cb1c9eadf7cd20a57babf3373edb0b03
Simon Yawin <simyawin@gmail.com>
2017-04-27 01:28:35 -0400

    Move code from P2PClient

--- Client.java
+++ Client.java
@@ -1,5 +1,5 @@
-import java.net.DatagramSocket;
-import java.net.InetAddress;
+import java.io.File;
+import java.net.*;
 import java.util.Scanner;
 
 /**
@@ -8,9 +8,174 @@ import java.util.Scanner;
 
 public class Client implements Sender, Receiver
 {
-    int portNumber;
-    String receiverHostname;
-    InetAddress receiverIP;
-    Scanner scan,seq;
-    DatagramSocket sendSkt;
+    private int portNumber;
+    private String receiverHostname;
+    private InetAddress receiverIP;
+    private Scanner scan,seq;
+//    private DatagramSocket sendSkt;
+
+    public Client(int portNumber, String receiverHostname) throws Exception
+    {
+        this.portNumber = portNumber;
+        this.receiverHostname = receiverHostname;
+        this.receiverIP = InetAddress.getByName(this.receiverHostname);
+    }
+
+    public void sendMessage(String msgType) throws Exception
+    {
+        DatagramSocket sendSkt = new DatagramSocket();
+        byte[] rcvData = new byte[1024];
+
+        System.out.println("Attempting to send to " + receiverIP);
+
+        String msg = genReqMsg(msgType);
+        String[] packets = Sender.makePacket(msg);
+
+        double estRTT = Sender.INIT_EST_RTT;
+        double devRTT = Sender.INIT_DEV_RTT;
+        devRTT = Sender.calcDevRTT(0.0, Sender.INIT_EST_RTT, estRTT, devRTT);
+        estRTT = Sender.calcEstimatedRTT(0.0, Sender.INIT_EST_RTT, estRTT);
+        int timeoutInterval = Sender.calcTimeoutInterval(estRTT, devRTT);
+
+        int currSeqNum = Sender.INIT_SEQ_NUM;
+
+        // Attempt to send pakets via UDP
+        for(String p : packets)
+        {
+            // Sending packets
+            byte[] sendData = new byte[p.length()];
+            sendData = p.getBytes();
+            DatagramPacket sendPkt = new DatagramPacket(sendData, sendData.length, receiverIP, portNumber);
+
+            try{
+                sendSkt.send(sendPkt);
+            }
+            catch (SocketException e){
+                System.out.println("Connection interrupted. Waiting for resumed connection.");
+            }
+
+            System.out.println("Sending following packet with sequence number " + Sender.getSeqNum(p) + "\n{\n" + p + "\n}\n");
+            System.out.println("Waiting for ACK...");
+
+            // Receiving ACKs
+            DatagramPacket rcvPkt;
+            while(true)
+            {
+                // Start Timer
+                sendSkt.setSoTimeout(timeoutInterval);
+                double startTime = System.nanoTime() / 1000000;
+                rcvPkt = new DatagramPacket(rcvData, rcvData.length);
+
+                // Try to receive a packet
+                try{
+                    sendSkt.receive(rcvPkt);
+                }
+                // If timeout
+                catch(SocketTimeoutException e){
+                    double endTime = System.nanoTime() / 1000000;
+                    System.out.println("Timeout!");
+                    // Recalculate timeout interval
+                    devRTT = Sender.calcDevRTT(startTime, endTime, estRTT, devRTT);
+                    estRTT = Sender.calcEstimatedRTT(startTime, endTime, estRTT);
+                    timeoutInterval = Sender.calcTimeoutInterval(estRTT, devRTT);
+                    // Resend packet
+                    try{
+                        sendSkt.send(sendPkt);
+                    }
+                    catch (SocketException se){
+                        System.out.println("Connection interrupted. Waiting for resumed connection.");
+                    }
+                    // Restart loop
+                    continue;
+                }
+                double endTime = System.nanoTime() / 1000000;
+                System.out.println("ACK received!");
+                //Recalculate timeout interval
+                devRTT = Sender.calcDevRTT(startTime, endTime, estRTT, devRTT);
+                estRTT = Sender.calcEstimatedRTT(startTime, endTime, estRTT);
+                timeoutInterval = Sender.calcTimeoutInterval(estRTT, devRTT);
+                // Check new sequence number
+                int newSeqNum = Sender.getSeqNum(new String(rcvPkt.getData()));
+                // If incorrect sequence number
+                if(newSeqNum != currSeqNum)
+                {
+                    System.out.println("Wrong sequence number.\nExpected " + currSeqNum + " got " + newSeqNum + ".");
+                    // Resend packet
+                    try{
+                        sendSkt.send(sendPkt);
+                    }
+                    catch (SocketException se){
+                        System.out.println("Connection interrupted. Waiting for resumed connection.");
+                    }
+                    // Restart loop
+                    continue;
+                }
+                // Turn off timer
+                sendSkt.setSoTimeout(0);
+                // Increment sequence number
+                currSeqNum = (currSeqNum + 1) % Sender.SEQ_NUM_WIN;
+                // End loop
+                break;
+            }
+
+            String ACK = new String(rcvPkt.getData());
+            int sequenceNum = Sender.getSeqNum(ACK);
+            int rcvpktsize = rcvPkt.getLength();
+            InetAddress clientIP = rcvPkt.getAddress();
+            int clientPort = rcvPkt.getPort();
+            String ack = new String(rcvPkt.getData());
+
+            System.out.println("ACK = \n[\n" + ack + "\n]");
+            System.out.println("Sequence number = " + sequenceNum);
+            System.out.println("Sender IP address: " + clientIP);
+            System.out.println("Sender port: " + clientPort);
+            System.out.println("Packet size: " + rcvpktsize + "\n\n\n");
+        }
+    }
+
+    /*
+	 * ---------------------------------------------------------------------------------------------
+	 * This method takes an integer parameter of flag, which will denote the message type to server
+	 * Message always begins with the flag number and the client's InetAddress, followed by CRLF
+	 *
+	 * Flag of 1 is for inform and update
+	 * 		This portion scans a users shared file --- chosen by GUI later on --- and appends
+	 * 		file name and file size (in bytes) of every file in the folder, followed by CRLF
+	 * Flag of 2 is to query for file(s)
+	 * Flag of 3 is for exit --- using "$$$$$$$$" as placeholder message for now
+	 *
+	 * Method returns the overall message String
+	 * ---------------------------------------------------------------------------------------------
+	 */
+    private static String genReqMsg(String msgType) throws Exception
+    {
+        Scanner scan = new Scanner(System.in);
+        String msg = "";
+
+        switch(msgType.toUpperCase())
+        {
+            case "INUP":
+                msg += "INUP " + InetAddress.getLocalHost() + "\r\n";
+                System.out.println("Please enter the absolute path of the sharing folder.");
+                File folder = new File(scan.next());
+                File[] sharedFiles = folder.listFiles();
+
+                for(File file : sharedFiles)
+                    msg += file.getName() + " " + file.length() + " " + file.getPath() + "\r\n";
+                break;
+            case "QUER":
+                msg += "QUER " + InetAddress.getLocalHost() + "\r\n";
+                System.out.println("Name of file to query? (Or type -showall to view entire directory");
+                String input = scan.next();
+                msg += input + "\r\n";
+                break;
+            case "EXIT":
+                msg += "EXIT " + InetAddress.getLocalHost() + "\r\n";
+                msg += "$$$$$$$$";
+                break;
+        }
+        msg += "\r\n";
+
+        return msg;
+    }
 }
