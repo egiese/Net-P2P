8fb9e818247f6094dc3670748e8d36fea8b9eef8
Simon Yawin <simyawin@gmail.com>
2017-04-26 20:41:55 -0400

    Added checking for Launcher

--- Launcher.java
+++ Launcher.java
@@ -9,12 +9,31 @@ public class Launcher
     public static void main(String[] args)
     {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Welcome to the P2P application.\nIs this machine a client [c] or a server [s]?");
-        String instanceType = scanner.next();
-        System.out.println("What port number will be used for communication?");
-        int port = scanner.nextInt();
-        System.out.println("What is this hostname of this machine?");
-        String hostName = scanner.next();
+        String instanceType,serverHostname = "";
+        int port;
+
+        System.out.println("Welcome to the P2P application.");
+        do
+        {
+            System.out.println("Is this machine a client [c] or a server [s]?");
+            instanceType = scanner.next();
+        } while (!(instanceType.equals("c") || instanceType.equals("s")));
+
+        if(instanceType.equals("c"))
+        {
+            do
+            {
+                System.out.println("What is the hostname of the server?");
+                serverHostname = scanner.next();
+            } while (serverHostname.isEmpty());
+        }
+
+        do
+        {
+            System.out.println("What port number will be used for communication?");
+            port = scanner.nextInt();
+        } while (port == 0);
+
     }
 
 }
